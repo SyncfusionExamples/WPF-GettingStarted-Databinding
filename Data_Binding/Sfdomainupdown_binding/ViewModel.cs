3529958db74d1dac87d1891932a45125edc97fff
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-10-28 17:49:07 +0530

    Added sample for data binding

---
+++ Data_Binding/Sfdomainupdown_binding/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfdomainupdown_binding
+{
+    public class ViewModel
+    {
+        private List<Employee> employees;
+        public List<Employee> Employees
+        {
+            get { return employees; }
+            set { employees = value; }
+        }
+        public ViewModel()
+        {
+            Employees = new List<Employee>();
+            populateItem();
+        }
+        private void populateItem()
+        {
+            Employees.Add(new Employee { Name = "Lucas", Email = "lucas@syncfusion.com" });
+            Employees.Add(new Employee { Name = "James", Email = "james@syncfusion.com" });
+            Employees.Add(new Employee { Name = "Jacob", Email = "jacob@syncfusion.com" });
+        }
+    }
+}
